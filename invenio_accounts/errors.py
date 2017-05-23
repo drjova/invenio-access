1196ef28bc7d40c967e47b97356a062ab1a5d77f
Harris Tzovanakis <me@drjova.com>
2017-05-23 15:41:01 +0200

    global: addition of JWT
    * Adds support for JWT (jwt.io), which is enabled by default. All the
      protected endpoints ``require_api_auth`` will be automatically
      check for JWT in the headers. A token can be generated by using the
      ``{{ jwt() }}`` context processor in jinja templates and can
      be send with each API request.
    
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

---
+++ invenio_accounts/errors.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2017 CERN.
+#
+# Invenio is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Utility function for ACCOUNTS."""
+
+from invenio_rest.errors import RESTException
+
+
+class JWTExtendedException(RESTException):
+    """Base exception for all JWT errors."""
+
+    code = 500
+
+
+class JWTDecodeError(JWTExtendedException):
+    """Exception raised when decoding is failed."""
+
+    code = 400
+    description = 'The JWT token has invalid format.'
+
+
+class JWTInvalidIssuer(JWTExtendedException):
+    """Exception raised when the user is not valid."""
+
+    code = 403
+    description = 'The JWT token is not valid.'
+
+
+class JWTExpiredToken(JWTExtendedException):
+    """Exception raised when JWT is expired."""
+
+    code = 403
+    description = 'The JWT token is expired.'
+
+
+class JWTInvalidHeaderError(JWTExtendedException):
+    """Exception raised when header argument is missing."""
+
+    code = 400
+    description = 'Missing required header argument.'
+
+
+class JWTNoAuthorizationError(JWTExtendedException):
+    """Exception raised when permission denied."""
+
+    code = 400
+    description = "The JWT token is not valid."
